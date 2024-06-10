f30e70d34658e0d428c804483296394e07023653
Bharath Sriraam R R <albharath.1305@gmail.com>
2024-06-09 21:58:37 -0400

    looks good

---
+++ app/api/crawl/route.ts
@@ -0,0 +1,163 @@
+import { Client } from "@notionhq/client";
+import Instructor from "@instructor-ai/instructor";
+import OpenAI from "openai";
+import { z } from "zod";
+import {
+  CreatePageParameters,
+  DatabaseObjectResponse,
+} from "@notionhq/client/build/src/api-endpoints";
+
+const stringTypes = ["rich_text", "title", "url"];
+
+const getUrlContent = async (url: string) => {
+  const res = await fetch(`https://r.jina.ai/${url}`);
+  const pageContent = await res.text();
+
+  return pageContent;
+};
+
+const getDbTitle = (db: DatabaseObjectResponse) => {
+  const firstTextNode = (db.title || []).find((item) => item.type === "text");
+
+  if (firstTextNode?.type === "text") return firstTextNode.text.content;
+
+  return "";
+};
+
+function extractDbIdFromUrl(url: string): string | null {
+  try {
+    const urlObj = new URL(url);
+    const path = urlObj.pathname;
+    const id = path.split("/")[1];
+    return id || null;
+  } catch (error) {
+    console.error("Invalid URL:", error);
+    return null;
+  }
+}
+
+export async function POST(request: Request) {
+  const req = await request.json();
+
+  const { databaseUrl, openAiKey, notionKey } = req;
+
+  const databaseId = extractDbIdFromUrl(databaseUrl);
+
+  if (!databaseId) throw new Error("Couldn't extract Notion database ID");
+
+  const openaiClient = new OpenAI({
+    apiKey: openAiKey,
+  });
+
+  const instructorClient = Instructor({
+    client: openaiClient,
+    mode: "FUNCTIONS",
+  });
+
+  const notion = new Client({
+    auth: notionKey,
+  });
+
+  const [pageContent, notionDb] = await Promise.all([
+    getUrlContent(req.url),
+    notion.databases.retrieve({
+      database_id: databaseId,
+    }),
+  ]);
+
+  const { properties } = notionDb;
+
+  const title = getDbTitle(notionDb as DatabaseObjectResponse);
+
+  const schema: {
+    [key: string]: any;
+  } = {};
+
+  Object.values(properties).forEach((prop) => {
+    if (!prop.name) return;
+
+    if (stringTypes.includes(prop.type)) {
+      schema[prop.name] = z.string();
+    }
+
+    if (prop.type === "multi_select") {
+      const options = prop.multi_select.options.map((item) => item.name);
+      schema[prop.name] = z.array(z.enum([options[0], ...options.slice(1)]));
+    }
+  });
+
+  const zodSchema = z.object(schema);
+
+  const res = await instructorClient.chat.completions.create({
+    messages: [{ role: "user", content: pageContent }],
+    model: "gpt-3.5-turbo",
+    response_model: {
+      schema: zodSchema,
+      name: title,
+    },
+    max_retries: 3,
+  });
+
+  const row: CreatePageParameters = {
+    parent: {
+      type: "database_id",
+      database_id: databaseId,
+    },
+    properties: {},
+  };
+
+  Object.values(properties).forEach((prop) => {
+    const { name, type } = prop;
+    const value = res[name];
+
+    if (!name || !res) return;
+
+    if (type === "rich_text") {
+      row.properties[name] = {
+        type,
+        [type]: [
+          {
+            text: {
+              content: value,
+            },
+          },
+        ],
+      };
+    }
+
+    if (type === "title") {
+      row.properties[name] = {
+        type,
+        [type]: [
+          {
+            text: {
+              content: value,
+            },
+          },
+        ],
+      };
+    }
+
+    if (type === "url") {
+      row.properties[name] = {
+        type,
+        [type]: value,
+      };
+    }
+
+    if (type === "multi_select") {
+      const options = (value as string[]).map((item) => ({ name: item }));
+
+      row.properties[name] = {
+        type,
+        [type]: options,
+      };
+    }
+  });
+
+  const result = await notion.pages.create(row);
+
+  return Response.json({
+    result,
+  });
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,184 @@
-import Image from "next/image";
+"use client";
+
+import { Loader2Icon } from "lucide-react";
+import toast, { Toaster } from "react-hot-toast";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
+import { z } from "zod";
+import { useState } from "react";
+
+const formSchema = z.object({
+  databaseUrl: z.string().url(),
+  url: z.string().url(),
+
+  openaiKey: z.string(),
+  notionKey: z.string(),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
+
+const crawl = async (values: FormSchema) => {
+  const res = await fetch("/api/crawl", {
+    method: "POST",
+    body: JSON.stringify(values),
+  });
+  const data = await res.json();
+  return data;
+};
 
 export default function Home() {
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      databaseUrl: "",
+      url: "",
+      openaiKey: "",
+      notionKey: "",
+    },
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+      await crawl(values);
+      toast.success("Added the page to your DB!", {
+        position: "bottom-center",
+      });
+    } catch (error) {
+      console.error(error);
+      toast.error(
+        "Could not add the page. Please check console for the error log",
+        {
+          position: "bottom-center",
+        }
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+    <main className="flex min-h-screen flex-col items-center gap-4 py-8 px-4">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-6 border rounded-md p-8"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          <h1 className="text-3xl font-bold tracking-tight text-center">
+            Notion AutoDB
+          </h1>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <FormField
+            control={form.control}
+            name="databaseUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Database URL</FormLabel>
+                <FormDescription>
+                  Expand the Database and paste the URL
+                </FormDescription>
+                <FormControl>
+                  <Input
+                    placeholder="https://www.notion.so/<YOUR-DB-ID>"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <FormField
+            control={form.control}
+            name="url"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Website URL</FormLabel>
+                <FormDescription>The website you want to add</FormDescription>
+                <FormControl>
+                  <Input placeholder="https://www.your-page.com" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <FormField
+            control={form.control}
+            name="openaiKey"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>OpenAI API Key</FormLabel>
+                <FormDescription>
+                  You can get your key{" "}
+                  <Link
+                    href="https://platform.openai.com/api-keys"
+                    target="_blank"
+                    className="underline"
+                  >
+                    here
+                  </Link>
+                </FormDescription>
+                <FormControl>
+                  <Input {...field} type="password" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="notionKey"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Notion API Key</FormLabel>
+                <FormDescription>
+                  You can get your key{" "}
+                  <Link
+                    href="https://developers.notion.com/docs/create-a-notion-integration#getting-started"
+                    target="_blank"
+                    className="underline"
+                  >
+                    here
+                  </Link>
+                </FormDescription>
+                <FormControl>
+                  <Input {...field} type="password" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="w-full" disabled={loading}>
+            {loading ? (
+              <>
+                <Loader2Icon className="animate-spin mr-2" /> Processing
+              </>
+            ) : (
+              "Submit"
+            )}
+          </Button>
+        </form>
+      </Form>
+
+      <Toaster />
     </main>
   );
 }
